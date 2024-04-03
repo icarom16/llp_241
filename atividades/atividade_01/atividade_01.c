6e6bc2bb8ce12d80dd974c2f01d813853dd9e22c
unknown <icaromarques1607@outlook.com>
2024-04-03 20:09:52 -0300

    atividade passada em sala

---
+++ atividades/atividade_01/atividade_01.c
@@ -0,0 +1,26 @@
+
+int main(void) {
+  printf("Hello World\n");
+int x = 100;
+  
+   printf("Digite a pontuação total do jogador: ");
+   scanf("%d" , &x);
+
+    if (x >= 0 && x <= 9)
+      printf("a pontuação obtida é insuficiente");
+  else 
+      if (x >= 10 && x <= 39)
+        printf("a pontuação obtida é regular");
+  else
+        if (x >= 40 && x <= 69)
+            printf("a pontuação obtida é bom");
+  else
+          if (x >= 70 && x <= 89)
+            printf("a pontuação obtida é ótimo");
+  else
+            if (x >= 90 && x <= 100)
+              printf("a pontuação obtida é excelente");
+     
+    
+  return 0;
+}
---
+++ atividades/atividade_02/atividade_02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int i = 0;
+ int rest = 0;
+
+void main() {
+    for(i=0;i<50;i++){
+        printf("%d\n", i);
+    };
+    return ;
+}
