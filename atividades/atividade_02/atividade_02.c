9b5812d511c5eb3746cbbb6cffed0d3800d15639
unknown <icaromarques1607@outlook.com>
2024-04-03 20:19:40 -0300

    primeiro commit

--- atividades/atividade_02/atividade_02.c
+++ atividades/atividade_02/atividade_02.c
@@ -3,7 +3,9 @@ int i = 0;
  int rest = 0;
 
 void main() {
-    for(i=0;i<50;i++){
+    for(i=0; i <= 50; i++){
+        int rest = i % 2;
+        if(rest == 0)
         printf("%d\n", i);
     };
     return ;
