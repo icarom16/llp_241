b8d3301ff0e07e899b1862b08c1b9666912a6a0b
icarom16 <icaromarques1607@outlook.com>
2024-09-16 22:42:57 -0300

    atualização avaliacao_05

--- avaliacao/avaliacao_05/Codigo.c
+++ avaliacao/avaliacao_05/Codigo.c
@@ -6,47 +6,38 @@
 int main() {
     FILE *arquivo;
     char linha[MAX_LINE];
-    int escolha;
 
-    // Criar arquivo em modo de escrita
     arquivo = fopen("meu_arquivo.txt", "w");
     if (arquivo == NULL) {
         printf("Erro ao criar arquivo!\n");
         return 1;
     }
 
-    // Menu para escolher se vai escrever mais algum texto
-    do {
-        printf("Digite um texto (ou 'sair' para fechar o arquivo):\n");
+    printf("Digite um texto (ou 'sair' para fechar o arquivo):\n");
+    while (1) {
         fgets(linha, MAX_LINE, stdin);
-        linha[strcspn(linha, "\n")] = 0; // Remover caractere de quebra de linha
-
-        if (strcmp(linha, "sair") != 0) {
-            fprintf(arquivo, "%s\n", linha);
-        } else {
+        linha[strcspn(linha, "\n")] = 0; 
+        if (strcmp(linha, "sair") == 0) {
             break;
         }
-    } while (1);
 
-    // Fechar arquivo
+        fprintf(arquivo, "%s\n", linha);
+    }
+
     fclose(arquivo);
 
-    // Abrir arquivo em modo de leitura
     arquivo = fopen("meu_arquivo.txt", "r");
     if (arquivo == NULL) {
         printf("Erro ao abrir arquivo!\n");
         return 1;
     }
 
-    // Mostrar cabeçalho
     printf(" \n======== Conteúdo do Arquivo ========\n");
 
-    // Ler cada linha do arquivo
     while (fgets(linha, MAX_LINE, arquivo) != NULL) {
         printf("%s", linha);
     }
 
-    // Fechar arquivo
     fclose(arquivo);
 
     return 0;
